bf17c1365d05cd78113fedfbc9230ac7a03aad1a
Hany Raaj <hanyraaj@gmail.com>
2021-03-06 09:19:17 +0600

    inventory, not found added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import './App.css';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import Review from './components/Review/Review';
 import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+        <Route exact path='/'>
+            <Shop></Shop>
+          </Route>
+          <Route path='/shop'>
+            <Shop></Shop>
+          </Route>
+          <Route path='/review'>
+            <Review></Review>
+          </Route>
+          <Route path='/inventory'>
+            <Inventory></Inventory>
+          </Route>
+          <Route path='*'>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
